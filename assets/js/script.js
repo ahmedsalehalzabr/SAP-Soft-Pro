df265a672637d3d555a12d6c002dbc16e66ea91b
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-03-13 04:19:10 -1200

    3/13

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,3 +159,163 @@ addEventOnElements(hoveredElements, "mouseout", function () {
     cursors[i].classList.remove("hovered");
   }
 });
+
+const translations = {
+  ar: {
+    home: "الصفحة الرئيسية",
+    
+    arabic: "AR",
+    english: "EN",
+  
+    1:"خدمات",
+    2:"معلومات عنا",
+    3:"المشاريع",
+    4:"مراجعة",
+    5:"للتواصل",
+    6:"ساب سوفت برو",
+    7:"الأنظمة وتطوير تطبيقات الويب وتطوير تطبيقات الهاتف المحمول والحلول المتكاملة",
+    8:"تطوير تطبيقات الويب",
+
+    9:"تطوير",
+    10:" تطوير الانظمة ",
+    11:"تطوير تطبيقات الموبايل",
+    12:"الحلول المتكاملة",
+    13:"رؤيتنا",
+    14:"رسالتنا",
+    15:"هدفنا",
+    16:"البرمجيات",
+    17:"في ساب سوفت برو، نحن نسعى لأن نكون الشركة الرائدة والموثوقة في مجال تقديم الحلول التكنولوجية المتكاملة. نؤمن بأن التكنولوجيا يمكن أن تكون قوة محركة للتغيير والتحسين، ونسعى جاهدين لتحويل هذه القوة إلى حلول مبتكرة وذكية تعزز عمليات الأعمال وتساهم في تحقيق النجاح لعملائنا.",
+    18:"من نحن",
+    19:"نحن شركة ساب سوفت برو، ونعتبر أنفسنا روادًا في مجال تطوير الأنظمة والتطبيقات وتقديم الحلول المتكاملة لعملائنا. نحن نجمع بين الخبرة العميقة والابتكار المستمر لتلبية احتياجات الأعمال المتنوعة والمساعدة في تحقيق أهدافها بكفاءة وفاعلية.",
+    20:"الاسم",
+    21:"م/مساعد فايز",
+    22:"رقم الهاتف",
+    23:"الايميل",
+    24:"المهنة",
+    25:"المدير العام",
+    26:"تتمثل رسالتنا في توفير حلول تكنولوجية مبتكرة وقوية تساعد عملائنا على تحقيق أهدافهم وتجاوز توقعاتهم. نحن نعمل على تطوير وتنفيذ أنظمة وتطبيقات متكاملة تلبي احتياجات الأعمال المتنوعة، بدءًا من المؤسسات الصغيرة وحتى الشركات الكبيرة والمؤسسات الحكومية. نحن نضمن تجربة ممتازة للمستخدم ونسعى لتحقيق رضا العملاء من خلال تقديم حلول ذات جودة عالية وخدمة عملاء استثنائية",
+    27:"هدفنا الأسمى هو تقديم حلول تكنولوجية متفوقة ومبتكرة تلبي تحديات عملائنا. نحن نعمل جنبًا إلى جنب مع الشركات والمؤسسات لفهم تمامًا احتياجاتهم وتوفير حلول مخصصة تعزز الكفاءة وتحسن الأداء وتمكِّنهم من التفوق في بيئة الأعمال المتنوعة والمتطورة.",
+    28:"اهم البرمجيات المستخدمة في عملنا- ساب سوفت برو",
+    29:"متوسط سعر النظام هو 500 دولار إلى 2000 دولار، ومتوسط سعر موقع الويب هو 200 دولار إلى 500 دولار، ومتوسط سعر تطبيق الويب هو 700 دولار إلى 1500 دولار، ومتوسط سعر تطبيق اندرويد للجوال هو 600 دولار إلى 3000 دولار.",
+    30:"م/احمد الزبر",
+    31:"مؤسس شركة ساب سوفت برو",
+    32:"ابقى على تواصل",
+    33:"ارسال ",
+    34:" 771143849 967+",
+    35:"صنعاء شارع القاهرة",
+    36:"حقوق النشر والتصميم بواسطة @ ساب سوفت برو - 2024",
+    37:"العودة إلى الأعلى",
+    38:" 776402091 967+",
+    39:" 771767919 967+",
+    40:"",
+    41:"",
+   
+  },
+    en: {
+      home: "Home",
+    
+      english: "EN",
+      arabic: "AR",
+      1:"Services",
+      2:"About Us",
+      3:"Project",
+      4:"Review",
+      5:"Contact",
+      6:"SAP Soft Pro",
+      7:"Systems, WebApp Development, MobileApp Development  and integrated solutions",
+      8:"Web App Development",
+      9:"Development",
+      10:"Systems Development",
+      11:"Mobile App Development",
+      12:"Integrated Solutions",
+      13:"Our Vision",
+      14:"Our Mission",
+      15:"Our Goal",
+      16:"Software",
+      17:"At SAP Soft Pro, we seek to be the leading and trusted company in providing integrated technology solutions. We believe that technology can be a driving force for change and improvement, and we strive to transform this power into innovative and intelligent solutions that enhance business operations and contribute to the success of our customers.",
+      18:"  Who are we.",
+      19:"We are SAP Soft Pro, and we consider ourselves leaders in the field of developing systems and applications and providing integrated solutions to our customers. We combine deep experience and continuous innovation to meet diverse business needs and help achieve their goals efficiently and effectively.",
+      20:"Name",
+      21:"Eng/Mas'ad Fayez",
+      22:"Phone Number",
+      23:"Email Address",
+      24:"Information",
+      25:"General Manager.",
+      26:"Our mission is to provide innovative and powerful technological solutions that help our customers achieve their goals and exceed their expectations. We develop and implement integrated systems and applications that meet the needs of diverse businesses, from small enterprises to large corporations and government institutions. We ensure excellent user experience and strive to achieve customer satisfaction by providing quality solutions and exceptional customer service",
+      27:" Our ultimate goal is to provide superior and innovative technological solutions that meet our customers' challenges. We work alongside businesses and organizations to fully understand their needs and provide customized solutions that enhance efficiency, improve performance and enable them to excel in a diverse and evolving business environment.",
+      28:"The most important software used in our work - SAP Soft Pro.",
+      29:"The average price of a system is $500 to $2000, the average price of a website is $200 to $500,The average price of a web app is $700 to $1,500, the average price of an Android mobile app is $600 to $3,000.",
+      30:"Eng/Ahmed Al-Zabr",
+      31:"Founder of SAP Soft Pro.co",
+      32:"Get In Touch",
+      33:"Send Message",
+      34:"+697 771143849",
+      35:"  Sanaa, Cairo Street",
+      36:"Copyright & Design By @Sap Soft Pro - 2024",
+      37:"Back To Top",
+      38:"+967 776402091",
+      39:"+967 771767919",
+      40:"",
+      41:"",
+     
+
+    },
+    
+  };
+
+/*const languageSelector = document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "ar"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
+*/
+
+
+const languageButton = document.getElementById("languageButton");
+languageButton.addEventListener("click", () => {
+  toggleLanguage();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "ar";
+  setLanguage(language);
+});
+
+const toggleLanguage = () => {
+  const currentLanguage = localStorage.getItem("lang") || "ar";
+  const newLanguage = currentLanguage === "ar" ? "en" : "ar";
+  setLanguage(newLanguage);
+  localStorage.setItem("lang", newLanguage);
+};
+
+
+
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    let translationValue = translations[language][translationKey];
+    
+    if (translationKey === "english") {
+      translationValue = language === "ar" ? "EN": "AR";
+    }
+    
+    element.textContent = translationValue;
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
